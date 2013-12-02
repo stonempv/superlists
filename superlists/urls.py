d5ec14356a94d700d4ca11982ce54bdb5c11f191
Michael Stone <stonempv@gmail.com>
2013-12-02 15:11:08 +0000

    First unit test and url mappying, dummy view

--- superlists/urls.py
+++ superlists/urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    
     # Examples:
-    # url(r'^$', 'superlists.views.home', name='home'),
+    url(r'^$', 'lists.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
